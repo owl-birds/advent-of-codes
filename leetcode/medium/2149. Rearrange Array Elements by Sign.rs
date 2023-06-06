7ce84cc79a2b11d3c144e54017d6efbe46645378
owl <75400842+owl-birds@users.noreply.github.com>
2023-06-07 00:46:44 +0700

    med 2149 edited

--- leetcode/medium/2149. Rearrange Array Elements by Sign.rs
+++ leetcode/medium/2149. Rearrange Array Elements by Sign.rs
@@ -1,15 +1,15 @@
 impl Solution {
     pub fn rearrange_array(nums: Vec<i32>) -> Vec<i32> {
         let mut result: Vec<i32> = Vec::new();
-        let mut positive_nums: Vec<Vec<i32>> = Vec::new();
-        let mut negative_nums: Vec<Vec<i32>> = Vec::new();
+        let mut positive_nums: Vec<i32> = Vec::new();
+        let mut negative_nums: Vec<i32> = Vec::new();
 
         for i in 0..nums.len(){
             if nums[i] > 0{
-                positive_nums.push(vec![nums[i], i as i32]);
+                positive_nums.push(nums[i]);
                 continue;
             }
-            negative_nums.push(vec![nums[i], i as i32]);
+            negative_nums.push(nums[i]);
         }
         println!("+:{:?}\n-:{:?}", positive_nums, negative_nums);
 
@@ -18,12 +18,12 @@ impl Solution {
         let mut is_pos: bool = true;
         for i in 0..nums.len(){
             if is_pos{
-                result.push(positive_nums[pos_idx][0]);
+                result.push(positive_nums[pos_idx]);
                 pos_idx += 1;
                 is_pos = false;
                 continue;
             }
-            result.push(negative_nums[neg_idx][0]);
+            result.push(negative_nums[neg_idx]);
             is_pos = true;
             neg_idx += 1;
         }
